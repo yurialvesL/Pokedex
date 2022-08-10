675092edc0579bef23b175f09b1e1b286679a8d2
yuri timao <yuri-timao2011@hotmail.com>
2022-08-10 14:19:09 -0300

    adjust in order pokedex

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from PyQt5.QtWidgets import QApplication
 
 qt = QApplication(sys.argv)
 poke = Pokedex()
-poke.poke_Type(152)
-poke.poke_description(152)
-poke.abilities_poke(152)
+poke.poke_Type(1)
+poke.poke_description(1)
+poke.abilities_poke(1)
 poke.show()
 sys.exit(qt.exec())
 
